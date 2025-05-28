a23dc9d220340e4cdee90ea407cd61c5457f52d7
Sergio <37486549+bigsergio13@users.noreply.github.com>
2025-05-28 10:35:28 -0300

    Update frete.js
    Habilitando o CORS para requisições no front-end

--- pages/api/frete.js
+++ pages/api/frete.js
@@ -3,6 +3,10 @@ const token = process.env.TOKEN_MELHORENVIO;
 console.log('API acessada')
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://scentimportados.com.br');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
